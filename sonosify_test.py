7fe113e8785db214f5762e77662cdf376e140f0b
simojenki <simojenki@users.noreply.github.com>
2021-09-29 18:55:05 +1000

    Support for downsampling alac

--- sonosify_test.py
+++ sonosify_test.py
@@ -115,7 +115,25 @@ class AudioFile:
         )
         return AudioFile(self.path.dirpath(new_file_name)) 
 
-  
+
+    def to_m4a(self):
+        # docker run -it -v "/tmp:/tmp" linuxserver/ffmpeg:latest -i "/tmp/01.flac" -vn -acodec alac -map_metadata -1 /tmp/01.m4a
+        new_file_name = "{uuid}{ext}".format(uuid=uuid(), ext=".m4a")
+        docker(
+            image="linuxserver/ffmpeg:latest",
+            docker_opts="-v {sounds}:/sounds".format(sounds=self.path.dirname),
+            entrypoint="ffmpeg",
+            args="""-i /sounds/{i} \
+                -vn \
+                -acodec alac \
+                /sounds/{o}""".format(
+                    i=self.path.basename, 
+                    o=new_file_name
+                )
+        )
+        return AudioFile(self.path.dirpath(new_file_name)) 
+
+
     def with_tags(self, artist="some-artist", title="some-title", track="some-track", album="some-album"):
         new_file_name = "{name}{ext}".format(name = uuid(), ext = self.path.ext)
         docker(
@@ -294,6 +312,7 @@ def test_flac_is_downsampled(
     flac = wav.to_flac(in_bits, in_freq).with_tags()
     flac_stream0 = flac.stream0()
 
+    assert flac_stream0["codec_name"] == "flac"
     assert flac_stream0["sample_fmt"] == in_bits
     assert flac_stream0["sample_rate"] == in_freq
     assert flac_stream0["bits_per_raw_sample"] == bits_per_raw_sample
@@ -302,6 +321,7 @@ def test_flac_is_downsampled(
     result = sonosify(flac)
     result_stream0 = result.stream0()
 
+    assert result_stream0["codec_name"] == "flac"
     assert result_stream0["sample_fmt"] == expected_bits
     assert result_stream0["sample_rate"] == expected_freq
     assert result_stream0["bits_per_raw_sample"] == expected_bits_per_raw_sample
@@ -310,5 +330,52 @@ def test_flac_is_downsampled(
 
     if(expected_stream_hash_match):
         assert result.stream_md5() == flac.stream_md5()
+
+
+@pytest.mark.parametrize(
+    "in_bits,in_freq,bits_per_raw_sample,expected_bits,expected_freq,expected_bits_per_raw_sample,expected_stream_hash_match", 
+    [
+        ("s16", "44100",  "16", "s16", "44100", "16", True), 
+        ("s16", "48000",  "16", "s16", "48000", "16", True), 
+        ("s16", "88200",  "16", "s16", "44100", "16", False), 
+        ("s16", "96000",  "16", "s16", "48000", "16", False), 
+
+        ("s32", "44100",  "24", "s32", "44100", "24", True), 
+        ("s32", "48000",  "24", "s32", "48000", "24", True), 
+        ("s32", "88200",  "24", "s32", "44100", "24", False), 
+        ("s32", "96000",  "24", "s32", "48000", "24", False), 
+    ]
+)
+def test_m4a_is_downsampled(
+    in_bits, 
+    in_freq, 
+    bits_per_raw_sample, 
+    expected_bits, 
+    expected_freq, 
+    expected_bits_per_raw_sample, 
+    expected_stream_hash_match, 
+    wav
+):
+    m4a = wav.to_flac(in_bits, in_freq).with_tags().to_m4a()
+    m4a_stream0 = m4a.stream0()
+
+    assert m4a_stream0["codec_name"] == "alac"
+    assert m4a_stream0["sample_fmt"] == "{}p".format(in_bits)
+    assert m4a_stream0["sample_rate"] == in_freq
+    assert m4a_stream0["bits_per_raw_sample"] == bits_per_raw_sample
+    assert len(m4a.tags()) > 1
+
+    result = sonosify(m4a)
+    result_stream0 = result.stream0()
+
+    assert result_stream0["codec_name"] == "flac"
+    assert result_stream0["sample_fmt"] == expected_bits
+    assert result_stream0["sample_rate"] == expected_freq
+    assert result_stream0["bits_per_raw_sample"] == expected_bits_per_raw_sample
+
+    assert len(result.tags()) == 1
+
+    if(expected_stream_hash_match):
+        assert result.stream_md5() == m4a.stream_md5()
 
 
